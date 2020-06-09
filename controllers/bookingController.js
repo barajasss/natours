3602475b8f1cd23daaf8be052aa7295e1ff2e473
Baraja Swargiary <barajaswargiary4444@gmail.com>
2020-06-09 07:46:54 +0530

    Fixed stripe problem

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
                 images: [
-                    `https://www.natours.dev/img/tours/${tour.imageCover}`,
+                    `https://natours-baraja.herokuapp.com/img/tours/${tour.imageCover}`,
                 ],
                 amount: tour.price * 100,
                 currency: 'usd',
@@ -44,7 +44,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async (session) => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items.amount / 100;
+    const price = session.display_items.amount / 100;
     await Booking.create({ tour, user, price });
 });
 
@@ -60,7 +60,7 @@ exports.webhookCheckout = (req, res, next) => {
     } catch (err) {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
-    if (event.type === 'checkout.session.complete') {
+    if (event.type === 'checkout.session.completed') {
         createBookingCheckout(event.data.object);
     }
     res.status(200).json({ received: true });
